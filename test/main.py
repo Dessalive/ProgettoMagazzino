5d82548fd300813096e60c115f7c28d31af5c556
Dessalive <francescodelsanto1@gmail.com>
2021-09-09 11:44:37 +0200

    Fix main.py

--- test/main.py
+++ test/main.py
@@ -1,35 +1,19 @@
 #!/usr/bin/env python
 
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    bott ="""
-    <form action="/frank" method="get">
-    <button type="bottone"> Next Page </button> 
-    """
+def home():
+    return render_template("index.html")
+#if __name__=="__main__":
+#  app.run()ott ="""
 
-    return bott #"<p>Hello, World!</p>"
-
-@app.route("/frank")
-def hello_frank():
-    a  = """
-    <html><header></header><body><h1>Benvenuto</h1><p>Inserisci il tuo nome e cognome.</p>
-
-     <form action="/action_page.php" method="get">
-  <label for="fname">Nome:</label>
-  <input type="text" id="fname" name="fname"><br><br>
-  <label for="lname">Cognome:</label>
-  <input type="text" id="lname" name="lname"><br><br>
-  <button type="submit">Submit</button>
-  <button type="submit" formmethod="post">Submit using POST</button>
-</form> 
-
-    </body></html>
-    """
-    return a
-
-
-    # <button type="bottone" formaction="/action_page2.php"> Next Page </button> 
+@app.route("/ricerca")
+def ricerca():
+   return render_template("ricerca.html") 
+  
+  
+  #<input type="text" id="fname" name="fname"><br><br>
+   
