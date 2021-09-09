5e17bdf454112f6020dd2fb5b5cc76fb0a785ebb
Dessalive <francescodelsanto1@gmail.com>
2021-09-09 15:02:01 +0200

    Aggiunta classe RicercaCodice

--- test/main.py
+++ test/main.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 
 from flask import Flask, render_template, url_for
+import csv
 
+
+database=[]
+temp=[]
 app = Flask(__name__)
 
 @app.route("/")
@@ -13,3 +17,28 @@ def home():
 def ricerca():
    return render_template("ricerca.html") 
   
+
+
+
+
+class RicercaCodice:
+    def __init__(self, codice, database, temp):
+        self.codice=codice
+        self.database=database
+        self.temp=temp
+
+    def Copia(self):
+        with open ("~/Desktop/ProgettoMagazzino/ProgettoMagazzino/product-template.csv","r") as x:
+            y = csv.reader(x, delimiter=',', quotechar='\"')
+            for i in y:
+                database.append("%s %s" % (i[1],i[7]))
+        return database
+    
+    def Cerca(self, database):
+        for j in database:
+            if #strvar in j.lower():
+                print("Articolo trovato: %s" % j.strip())
+                temp.append(j.strip())
+        
+        print("Sono stati trovati " + len(temp) + " articoli.")                 
+    
