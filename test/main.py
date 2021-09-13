7e88c6ff1180733122a4ca2e7fe5f7f9ef39b108
Dessalive <francescodelsanto1@gmail.com>
2021-09-13 10:30:11 +0200

    Upload main.py

--- test/main.py
+++ test/main.py
@@ -1,35 +1,37 @@
 #!/usr/bin/env python
 
 
+from typing import Any
 from flask import Flask, render_template, url_for, request
 import csv
 
 
-#
-#temp=[]
-#
-#
-#class RicercaCodice:
-#    def __init__(self, codice, database):
-#        self.codice=codice
-#        self.database=database
-#
-#    def copia(self, database):
-#        database=[]
-#        with open ("~/Desktop/ProgettoMagazzino/ProgettoMagazzino/product-template.csv","r") as x:
-#            y = csv.reader(x, delimiter=',', quotechar='\"')
-#            for i in y:
-#                database.append("%s %s" % (i[1],i[7]))
-#        return database
-#    
-#    def cerca(self, database, codice):
-#        for j in database:
-#            if codice in j.lower():
-#                print("Articolo trovato: " + str(j.strip()))
-#                temp.append(j.strip())
-#        
-#        print("Sono stati trovati " + str(len(temp)) + " articoli.")   
-#
+
+
+
+class CodiceArticolo:
+    def __init__(self, codice, database):
+        self.codice=codice
+        self.database=database
+
+    def copia(self, database):
+        database=[]
+        with open ("~/Desktop/ProgettoMagazzino/ProgettoMagazzino/product-template.csv","r") as x:
+            y = csv.reader(x, delimiter=',', quotechar='\"')
+            for i in y:
+                database.append("%s %s" % (i[1],i[7]))
+        return database
+    
+    def cerca(self, database, codice):
+
+        temp=[]
+        for j in database:
+            if codice in j.lower():
+                print("Articolo trovato: " + str(j.strip()))
+                temp.append(j.strip())
+        
+        print("Sono stati trovati " + str(len(temp)) + " articoli.")   
+
 
 
 app = Flask(__name__)
@@ -37,20 +39,21 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def home():
 
+def home(database):
+
+    CodiceArticolo.copia(database=[])
     
     return render_template("index.html")
 
 
 @app.route("/ricerca", methods = ['GET'])
-def ricerca():
+def ricerca(database, codice):
     
-    print(request.form.get('codice'))
-    
+    CodiceArticolo.cerca(database, codice)
     
 
-    return render_template("ricerca.html") 
+    return render_template("ricerca.html", database = database, codice = codice) 
   
 
 
